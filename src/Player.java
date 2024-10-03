d0027164660e8bfa1bccd6c5ec67f3b7609f63e8
Christian-Wan <christian.wan295@gmail.com>
2024-10-03 16:44:42 -0400

    Parsing works now and hitbox detection works

--- src/Player.java
+++ src/Player.java
@@ -10,14 +10,15 @@ public class Player {
 
     public Player(Engine engine) {
         this.engine = engine;
-        hitBox = new Rectangle(10, 10);
-        speed = 2;
+        hitBox = new Rectangle(xCord, yCord, 10, 10);
+        speed = 4;
         xCord = 10;
         yCord = 10;
     }
 
     public void updatePlayer() {
         movePlayer();
+        interact();
     }
 
     public void movePlayer() {
@@ -28,38 +29,38 @@ public class Player {
         int right = input.indexOf(KeyEvent.VK_D);
 
         if (up > left && up > down && up > right) {
-            System.out.println("up");
             yCord -= speed;
         }
         else if (left > up && left > down && left > right) {
-            System.out.println("left");
             xCord -= speed;
         }
         else if (down > left && down > up && down > right) {
-            System.out.println("down");
             yCord += speed;
         }
         else if (right > left && right > down && right > up) {
-            System.out.println("right");
             xCord += speed;
         }
+        hitBox.setLocation(xCord, yCord);
     }
 
     public void interact() {
+
         for (int i = 0; i < engine.getStage().getInteractables().size(); i++) {
+
             if (engine.getStage().getInteractables().get(i).getInteractBox().contains(hitBox)) {
-               if (engine.getStage().getInteractables().get(i) instanceof NPC) {
-                   //SHOW TEXT ABOVE PLAYER HEAD THAT STATES THE PLAYER CAN PRESS E TO INTERACT
-                   System.out.println("NPC HERE");
-               }
-               else {
-                   if (engine.getStage().getInteractables().get(i).getInteractBox().getX() > 60) {
-                       System.out.println("GO NEXT STAGE");
-                   }
-                   else {
-                       System.out.println("GO PREVIOUS");
-                   }
-//                   engine.setStage(new Stage());
+                System.out.println("test");
+                if (engine.getStage().getInteractables().get(i) instanceof NPC) {
+                    //SHOW TEXT ABOVE PLAYER HEAD THAT STATES THE PLAYER CAN PRESS E TO INTERACT
+                    System.out.println("NPC HERE");
+                }
+                else {
+                    if (engine.getStage().getInteractables().get(i).getInteractBox().getX() > 60) {
+                        System.out.println("GO NEXT STAGE");
+                    }
+                    else {
+                        System.out.println("GO PREVIOUS");
+                    }
+//                      engine.setStage(new Stage());
                }
             }
         }
--- src/Stage.java
+++ src/Stage.java
@@ -1,8 +1,11 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Stage {
@@ -14,12 +17,14 @@ public class Stage {
     public Stage(Engine engine) {
         this.engine = engine;
         currentMap = "map1";
+        interactables = new ArrayList<>();
         parseMap();
     }
 
     public Stage(Engine engine, String map) {
         this.engine = engine;
         currentMap = map;
+        interactables = new ArrayList<>();
         parseMap();
     }
 
@@ -29,8 +34,14 @@ public class Stage {
     }
 
     public void draw(Graphics2D g) {
+        String path = "images/" + currentMap + ".png";
+        BufferedImage background = null;
+        try {
+            background = ImageIO.read(new File(path));
+        } catch (IOException e) {}
+        g.drawImage(background, 0, 0, 1280, 640, null);
         for (int i = 0; i < interactables.size(); i++) {
-            g.drawRect((int) interactables.get(i).getInteractBox().getX(),(int) interactables.get(i).getInteractBox().getX(),(int) interactables.get(i).getInteractBox().getX(),(int) interactables.get(i).getInteractBox().getX());
+            g.drawRect((int) interactables.get(i).getInteractBox().getX(),(int) interactables.get(i).getInteractBox().getY(),(int) interactables.get(i).getInteractBox().getWidth(),(int) interactables.get(i).getInteractBox().getHeight());
         }
     }
 
@@ -39,8 +50,8 @@ public class Stage {
     }
 
     private void parseMap() {
-        String path = "map_data" + currentMap;
-        File f = new File("map_data/");
+        String path = "map_data/" + currentMap;
+        File f = new File(path);
         Scanner s = null;
         String line;
         String[] split;
@@ -53,14 +64,14 @@ public class Stage {
         while(s.hasNext()) {
             line = s.nextLine();
             split = line.split(", ");
-
+            System.out.println(Arrays.toString(split));
             if (split.length == 4) {
                 //x, y, height, width
                 interactables.add(new Interactable(Integer.parseInt(split[0]), Integer.parseInt(split[1]), Integer.parseInt(split[2]), Integer.parseInt(split[3])));
             }
             else {
                 //x, y, height, width, name
-                interactables.add(new NPC(Integer.parseInt(split[1]), Integer.parseInt(split[2]), Integer.parseInt(split[3]), Integer.parseInt(split[4]), split[0]));
+                interactables.add(new NPC(Integer.parseInt(split[0]), Integer.parseInt(split[1]), Integer.parseInt(split[2]), Integer.parseInt(split[3]), split[4]));
             }
         }
 
